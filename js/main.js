5c068cfe91ec7f5b59810789d14d1de504a242d3
awresh <experimentsworlds@gmail.com>
2022-12-06 16:37:19 +0530

    portfolio

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//--------------------------------about section tab-----------------
+console.log("inner_yes");
+(()=>{
+    let aboutSection=document.querySelector(".about-section"),
+    tabsContainer=document.querySelector(".about-tabs");
+
+    tabsContainer.addEventListener("click",(event)=>{
+        let target=event.target.getAttribute("data-target");
+            console.log(target)
+        if(event.target.classList.contains("tab-item") && !event.target.classList.contains("active")){
+            let target=event.target.getAttribute("data-target");
+            console.log(target)
+            tabsContainer.querySelector(".active").classList.remove("outer-shadow","active");
+            event.target.classList.add("active","outer-shadow");
+
+            aboutSection.querySelector(".tab-content.active").classList.remove("active");
+
+            aboutSection.querySelector(target).classList.add("active")
+        }
+    })
+})();
