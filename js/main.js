5c325d8defa6f18d7d8697c2da0e6bd3dfcf1482
awresh <experimentsworlds@gmail.com>
2022-12-08 00:35:07 +0530

    pf

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
     tabsContainer=document.querySelector(".about-tabs");
 
     tabsContainer.addEventListener("click",(event)=>{
-        let target=event.target.getAttribute("data-target");
+      //  let target=event.target.getAttribute("data-target");
         if(event.target.classList.contains("tab-item") && !event.target.classList.contains("active")){
             let target=event.target.getAttribute("data-target");
             tabsContainer.querySelector(".active").classList.remove("outer-shadow","active");
@@ -12,6 +12,32 @@
 
             aboutSection.querySelector(".tab-content.active").classList.remove("active");
 
+            aboutSection.querySelector(target).classList.add("active")
+            console.log(target)
+        }
+    })
+})();
+
+
+
+
+
+
+(()=>{
+    let aboutSection=document.querySelector(".portfolio-section"),
+    tabsContainer=document.querySelector(".portfolio-filter");
+    
+
+    tabsContainer.addEventListener("click",(event)=>{
+        let target=event.target.getAttribute("data-target");
+        if(event.target.classList.contains("filter-item") && !event.target.classList.contains("active")){
+            let target=event.target.getAttribute("data-target");
+            console.log("yes")
+            tabsContainer.querySelector(".active").classList.remove("outer-shadow","active");
+            event.target.classList.add("active","outer-shadow");
+
+            aboutSection.querySelector(".row.portfolio-items.active").classList.remove("active");
+            console.log(target)
             aboutSection.querySelector(target).classList.add("active")
         }
     })
